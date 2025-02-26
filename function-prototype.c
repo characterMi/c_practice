52d30c8029d696283022b35ef3af34bfe98af427
characterMi <abol1385fx@gmail.com>
2025-02-26 22:55:49 +0330

    function prototypes

---
+++ function-prototype.c
@@ -0,0 +1,13 @@
+void func(char[], int);
+
+int main()
+{
+    // Function prototype is like defining the type of a function and its arguments, like we did, we set the type of the function at the top and then we defined the function itself at the end.
+
+    return 0;
+}
+
+void func(char name[], int age)
+{
+    // do something.
+}
